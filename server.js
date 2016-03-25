22520b5cb46ca4c64fbfded154beb572cc35ae8c
2JN <jesquivelnavas@gmail.com>
2016-03-24 20:42:02 -0600

    Replace the callback functions with events, to make the code more readable

--- server.js
+++ server.js
@@ -5,9 +5,11 @@ const http = require('http')
 //command = PORT = 8080 node server.js
 const port = process.env.PORT || 8080
 
-const server = http.createServer(onRequest)
+const server = http.createServer()
+server.listen(port)
 
-server.listen(port, onListening)
+server.on('request', onRequest)
+server.on('listening', onListening)
 
 function onRequest(req, res){
     res.end("Hello node.js")
