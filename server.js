9b0734dc46ee20d5c8600491fbd7d132db855232
2JN <jesquivelnavas@gmail.com>
2016-03-24 20:35:50 -0600

    Add named functions to replace the anonymous ones, the reason is to prevent the 'callback hell'

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 'use strict'
 
 const http = require('http')
+
 //command = PORT = 8080 node server.js
 const port = process.env.PORT || 8080
 
-const server = http.createServer(function(req, res){
-    res.end("Hola node.js")
-})
+const server = http.createServer(onRequest)
 
-server.listen(port, function(){
-    console.log('Servidor escuchando en el puerto ' + port)
-})
+server.listen(port, onListening)
+
+function onRequest(req, res){
+    res.end("Hello node.js")
+}
+
+function onListening(){
+    console.log('Server running in port ' + port)
+}
