77b8250a4b12d4b180cd09fed61328a3112d3963
2JN <jesquivelnavas@gmail.com>
2016-03-24 20:28:55 -0600

    Added the answer to the server through callback functions

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const http = require('http')
 //command = PORT = 8080 node server.js
 const port = process.env.PORT || 8080
 
-const server = http.createServer()
+const server = http.createServer(function(req, res){
+    res.end("Hola node.js")
+})
 
-server.listen(port)
+server.listen(port, function(){
+    console.log('Servidor escuchando en el puerto ' + port)
+})
